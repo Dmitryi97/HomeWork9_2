8e45b1f7615d1eeeb2c03de3a23f82ccc12cf7e2
Dmitryi <pytin97@bk.ru>
2024-05-05 17:30:50 +0300

    Initial commit!

---
+++ HomeWork6_2/src/test/java/pro/sky/HomeWork6_2/HomeWork62ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.HomeWork6_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork62ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
