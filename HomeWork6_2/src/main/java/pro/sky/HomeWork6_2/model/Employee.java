8f867492d45b73ea416b1550300de332c8cc361b
Dmitryi <pytin97@bk.ru>
2024-05-13 21:46:51 +0300

    Выполнил ДЗ!

--- HomeWork6_2/src/main/java/pro/sky/HomeWork6_2/model/Employee.java
+++ HomeWork6_2/src/main/java/pro/sky/HomeWork6_2/model/Employee.java
@@ -5,11 +5,15 @@ import java.util.Objects;
 public class Employee {
     private String firstName;
     private String lastName;
+    private int department;
+    private double salary;
 
 
-    public Employee(String firstName, String lastName) {
+    public Employee(String firstName, String lastName, int department, double salary) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.department = department;
+        this.salary = salary;
     }
 
     public Employee() {
@@ -23,17 +27,38 @@ public class Employee {
         return lastName;
     }
 
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName);
+        return Objects.equals(firstName, employee.firstName)
+                && Objects.equals(lastName, employee.lastName)
+                && Objects.equals(department, employee.department)
+                && Objects.equals(salary, employee.salary);
     }
 
     @Override
     public String toString() {
         return "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'';
+                ", lastName='" + lastName + '\'' +
+                ", department='" + department + '\'' +
+                ", salary'" + salary;
     }
 }
